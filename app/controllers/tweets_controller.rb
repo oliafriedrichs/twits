948ba408b1c78652b20f1314a1a756f323026233
Olia Friedrichs <oliafriedrichs@icloud.com>
2017-03-20 20:29:22 -0500

    Added: *Tweets form *Ability to edit tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,24 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_tweet, only: [:show, :edit, :update]
   def index
+    @tweets = Tweet.all
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to @tweet, notice: "Your tweet was updated!"}
+      else
+        format.html {render :edit}
+      end
+    end
   end
 
   def new
@@ -33,5 +43,8 @@ private
     params.require(:tweet).permit(:message, :user_id)
   end
 
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
 end
