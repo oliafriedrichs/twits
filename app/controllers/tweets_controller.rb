29e5954e180983e210f537f9e1cd83b13a6ab0c5
Olia Friedrichs <oliafriedrichs@icloud.com>
2017-03-20 19:28:55 -0500

    Add ability to create and read tweets
    *Add new and create methods / views
    *Add show view tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,37 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def edit
   end
 
   def new
+    @tweet = Tweet.new
   end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: 'Tweet Out!'}
+      else
+        format.html { render :new}
+      end
+    end
+  end
+
+private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
+
+
 end
