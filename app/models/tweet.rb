99eb4ad9cb069e99ecc2792448f7d3bb2349e876
Olia Friedrichs <oliafriedrichs@icloud.com>
2017-03-22 20:09:43 -0500

    Add Likes table and set up model relationships

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  has_many :likes
 
   validates :message, presence: true, length: {maximum: 140, too_long: "Tweets are 140 characters MAX"}
 end
