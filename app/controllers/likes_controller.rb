dc3d16149e2596636179a838b05c1cab960a0a16
Olia Friedrichs <oliafriedrichs@icloud.com>
2017-03-22 20:42:17 -0500

    Add unlike tweet function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,9 +6,18 @@ class LikesController < ApplicationController
     @tweet.likes.where(user_id: current_user.id).first_or_create
 
     respond_to do |format|
-      format.html { redirect_to request.referrer}
+      format.html { redirect_to request.referrer }
     end
   end
+
+  def destroy
+    @tweet.likes.where(user_id: current_user.id).destroy_all
+
+    respond_to do |format|
+      format.html { redirect_to request.referrer }
+    end
+  end
+
 
   private
 
